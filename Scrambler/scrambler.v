1131bf049827961fb2e675c58563a4995da88b63
Baiquni <daqunster@vt.edu>
2023-03-18 12:10:02 -0400

    Revised scrambler to meet 3GPP standard

--- Scrambler/scrambler.v
+++ Scrambler/scrambler.v
@@ -1,27 +1,39 @@
-module scrambler (
+module nr_scrambler (
     input wire clk,
     input wire reset,
     input wire enable,
-    input wire [31:0] seed,
+    input wire [30:0] c_init,
     input wire data_in,
     output reg data_out
 );
 
-reg [30:0] lfsr;
+reg [30:0] x1, x2;
 
-// Update LFSR on the rising edge of the clock
+integer n;
+
+// Gold sequence generator LFSR update function
+function [30:0] lfsr_update;
+    input [30:0] lfsr;
+    lfsr_update = {lfsr[29:0], lfsr[30] ^ lfsr[27]};
+endfunction
+
+// Scrambling initialization
 always @(posedge clk) begin
     if (reset) begin
-        lfsr <= seed;
-    end else if (enable) begin
-        lfsr <= {lfsr[29:0], lfsr[30] ^ lfsr[27]};
+        x1 <= 31'b1000000000000000000000000000000;
+        x2 <= c_init;
     end
 end
 
-// Scramble the input data using the LFSR output
+// Generate the Gold sequence and scramble the input data
 always @(posedge clk) begin
     if (enable) begin
-        data_out <= data_in ^ lfsr[30];
+        // Update LFSRs
+        x1 <= lfsr_update(x1);
+        x2 <= lfsr_update(x2);
+        
+        // Generate the Gold sequence and scramble the input data
+        data_out <= data_in ^ (x1[30] ^ x2[30]);
     end
 end
 
